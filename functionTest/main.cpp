b3ee3fabeefd00fa9edcc8be26e2f0f3829c1b51
Carlos Gomez <armarpc@gmail.com>
2021-05-23 20:07:43 -0400

    Initial Commit

---
+++ functionTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  functionTest
+//
+//  Created by Carlos on 23/5/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
