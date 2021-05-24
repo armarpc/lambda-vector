7e6f2f757a35ab49cb5b1c34420f03aa6b85a131
Carlos Gomez <armarpc@gmail.com>
2021-05-23 20:36:57 -0400

    init

--- functionTest/main.cpp
+++ functionTest/main.cpp
@@ -6,9 +6,47 @@
 //
 
 #include <iostream>
+#include <vector>
+
+std::vector<std::function<void(void)>> actions;
+
+void funcA(void) {
+    std::cout << "A" << std::endl;
+}
+
+void funcB(void) {
+    std::cout << "B" << std::endl;
+}
+
+void funcC(std::vector<int> v) {
+    for (auto d : v) {
+        std::cout << d << std::endl;
+    }
+}
+
+void dispatch(std::function<void(void)> a) {
+    actions.push_back(a);
+}
+
+void performActions() {
+    for (auto a : actions) {
+        a();
+    }
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    
+    dispatch(funcA);
+    
+    dispatch(funcB);
+    
+    std::vector<int> v = {1,2,3,4,5};
+    dispatch([=]{
+        funcC(v);
+    });
+    
     std::cout << "Hello, World!\n";
+    
+    performActions();
     return 0;
 }
